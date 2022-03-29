3c0ee012799e517857a840985f9c36e077e9d4d7
willook <jongwhoa.lee@gmail.com>
2022-03-29 18:22:13 +0900

    fix typo in typing hint

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from  typing import Tuple, List, Dict
+from typing import Tuple, List, Dict
 
 import numpy as np
 
 from dtw import accelerated_dtw
 from motion_embedding import MotionEmbedding
 
-def compute_standard_action_database(data_path) -> Dict(str, List(MotionEmbedding)):
+def compute_standard_action_database(data_path) -> Dict[str, List[MotionEmbedding]]:
     std_db = {}
     # dummy
     feat1_1 = MotionEmbedding()
@@ -35,20 +35,33 @@ def motion_encode(keypoints) -> MotionEmbedding:
     """
     # dummy
     motion_embedding = MotionEmbedding()
-    motion_embedding.init_by_random
+    motion_embedding.init_by_random()
     return motion_embedding
 
-def compute_action_similarities(motion_embedding: MotionEmbedding, std_db: Dict(str, List(MotionEmbedding))) -> Dict(str, List(float)):
+def compute_action_similarities(anchor: MotionEmbedding, std_db: Dict[str, List[MotionEmbedding]]) -> Dict[str, List[float]]:
     """
-    param motion_embedding: reference motion embedding to recognize
+    param anchor: reference motion embedding to recognize
     param std_db: standard action database
     return Dict(str, List(float) similarities: Averages of similarity for each body part between reference motion embedding and each motion embedding of std_db.
     Similarity for each body part is computed by average cosine similarity between the embedding pairs in path. 
     """
     # use accelerated_dtw
-    pass
+    action_similarities: Dict[str, List[float]] = {} 
+    for action_label, motion_embeddings in std_db.items():
+        if not action_label in action_similarities:
+            action_similarities[action_label] = []
+        for motion_embedding in motion_embeddings:
+            # 평균값인지 합인지 확인하기
+            torso_similarity = accelerated_dtw(anchor.torso, motion_embedding.torso)
+            left_leg_similarity = accelerated_dtw(anchor.left_leg, motion_embedding.left_leg)
+            right_leg_similarity = accelerated_dtw(anchor.right_leg, motion_embedding.right_leg)
+            left_arm_similarity = accelerated_dtw(anchor.left_arm, motion_embedding.left_arm)
+            right_arm_similarity = accelerated_dtw(anchor.right_arm, motion_embedding.right_arm)
+            similarity = np.mean([torso_similarity, left_leg_similarity, right_leg_similarity, left_arm_similarity, right_arm_similarity])
+            action_similarities[action_label].append(similarity)
+    return action_similarities
 
-def predict_action(motion_embedding: MotionEmbedding, std_db: Dict(str, List(MotionEmbedding))) -> Tuple(str, float):
+def predict_action(motion_embedding: MotionEmbedding, std_db: Dict[str, List[MotionEmbedding]]) -> Tuple[str, float]:
     """
     return action label, similarities
     Predict action based on similarities.
