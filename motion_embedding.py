8999b5642479ba97093ca42af213c3a9876a65e0
willook <jongwhoa.lee@gmail.com>
2022-03-29 19:02:04 +0900

    Add time axis

--- motion_embedding.py
+++ motion_embedding.py
@@ -8,9 +8,9 @@ class MotionEmbedding:
     right_leg = None
 
     #def __init__(self, torso, left_arm, right_arm, left_leg, right_leg, random_init=False):
-    def init_by_random(self):
-        self.torso = np.random.rand(128)
-        self.left_arm = np.random.rand(64)
-        self.right_arm = np.random.rand(64)
-        self.left_leg = np.random.rand(64)
-        self.right_leg = np.random.rand(64)
+    def init_by_random(self, T = 32):
+        self.torso = np.random.rand(T, 128)
+        self.left_arm = np.random.rand(T, 64)
+        self.right_arm = np.random.rand(T, 64)
+        self.left_leg = np.random.rand(T, 64)
+        self.right_leg = np.random.rand(T, 64)
