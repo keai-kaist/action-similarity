d3d86c576f64839b11e238361dd28a75a5cb4c46
willook <jongwhoa.lee@gmail.com>
2022-04-15 23:26:15 +0900

    Add cache file function

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import os
+import pickle
+
 import numpy as np
 
 def euclidean_distances(x: any, y: any) -> any:
@@ -8,3 +11,13 @@ def manhattan_distances(x: any, y: any) -> any:
     return np.linalg.norm(x - y, ord=1)
     #return np.abs(np.subtract(x, y)).sum()
 
+def cache_file(file_name: str, func, *args, **kargs):
+    file_name = file_name.rstrip(".mp4") + ".pickle"
+    if os.path.exists(file_name):
+        with open(file_name, "rb") as f:
+            data = pickle.load(f)
+    else:
+        data = func(*args, **kargs)
+        with open(file_name, "wb") as f:
+            pickle.dump(data, f)
+    return data
